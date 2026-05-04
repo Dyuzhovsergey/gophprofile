37a194b453f6ec39d7a0949dd89fa61d57b85eda
Sergey <dyuzhovsergey@gmail.com>
2026-05-04 15:43:15 +0300

    add connect application to PostgreSQL

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+
 	"github.com/Dyuzhovsergey/gophprofile/internal/config"
 	"github.com/Dyuzhovsergey/gophprofile/internal/logger"
+	"github.com/Dyuzhovsergey/gophprofile/internal/repository/postgres"
 	"go.uber.org/zap"
 )
 
@@ -17,8 +20,16 @@ func main() {
 		_ = log.Sync()
 	}()
 
+	db, err := postgres.NewPool(context.Background(), cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatal("failed to connect to postgres", zap.Error(err))
+	}
+	defer db.Close()
+
 	log.Info(
 		"GophProfile worker started",
 		zap.String("log_level", cfg.LogLevel),
 	)
+
+	log.Info("connected to postgres")
 }
