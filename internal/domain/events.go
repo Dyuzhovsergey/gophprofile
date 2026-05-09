309361fa6a363a85800c0940ccc30c79d73f181e
Sergey <dyuzhovsergey@gmail.com>
2026-05-09 10:13:26 +0300

    added domain event delete avatar

--- internal/domain/events.go
+++ internal/domain/events.go
@@ -6,3 +6,11 @@ type AvatarUploadEvent struct {
 	UserID   string `json:"user_id"`
 	S3Key    string `json:"s3_key"`
 }
+
+// AvatarDeletedEvent описывает событие удаления аватарки.
+type AvatarDeletedEvent struct {
+	AvatarID        string                   `json:"avatar_id"`
+	UserID          string                   `json:"user_id"`
+	S3Key           string                   `json:"s3_key"`
+	ThumbnailS3Keys map[ThumbnailSize]string `json:"thumbnail_s3_keys"`
+}
