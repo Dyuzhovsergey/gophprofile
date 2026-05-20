4c3fab73044930d4635afa2196c1db8dc35caff9
Sergey <dyuzhovsergey@gmail.com>
2026-05-20 12:02:37 +0300

    Propagate trace context through RabbitMQ

--- internal/broker/rabbitmq/publisher.go
+++ internal/broker/rabbitmq/publisher.go
@@ -14,7 +14,6 @@ import (
 	observabilitytracing "github.com/Dyuzhovsergey/gophprofile/internal/observability/tracing"
 	"github.com/google/uuid"
 	amqp "github.com/rabbitmq/amqp091-go"
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 )
 
@@ -40,55 +39,6 @@ func rabbitMQPublishAttrs(
 	result = append(result, attrs...)
 
 	return result
-}
-
-// amqpTableCarrier адаптирует RabbitMQ headers под OpenTelemetry TextMapCarrier.
-type amqpTableCarrier struct {
-	headers amqp.Table
-}
-
-// Get возвращает значение header-а.
-func (c amqpTableCarrier) Get(key string) string {
-	value, ok := c.headers[key]
-	if !ok {
-		return ""
-	}
-
-	switch typedValue := value.(type) {
-	case string:
-		return typedValue
-	case []byte:
-		return string(typedValue)
-	default:
-		return fmt.Sprint(typedValue)
-	}
-}
-
-// Set записывает header.
-func (c amqpTableCarrier) Set(key string, value string) {
-	c.headers[key] = value
-}
-
-// Keys возвращает список header-ключей.
-func (c amqpTableCarrier) Keys() []string {
-	keys := make([]string, 0, len(c.headers))
-	for key := range c.headers {
-		keys = append(keys, key)
-	}
-
-	return keys
-}
-
-// injectTraceHeaders сохраняет текущий trace context в RabbitMQ headers.
-func injectTraceHeaders(ctx context.Context) amqp.Table {
-	headers := amqp.Table{}
-
-	otel.GetTextMapPropagator().Inject(
-		ctx,
-		amqpTableCarrier{headers: headers},
-	)
-
-	return headers
 }
 
 const (
